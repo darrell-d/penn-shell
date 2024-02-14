5ff2495c11190620205db9716560dfd12219f0bf
Darrell De Freitas <ddd@seas.upenn.edu>
2024-02-13 23:29:41 -0500

    wait_pid added

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE 1
+#endif
+
 #define BUF_LEN 4096U
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include "helpers.h"
 #include "parser.h"
@@ -43,33 +52,49 @@ int main() {
 
     struct parsed_command* cmd = NULL;
 
-    // struct parsed_command {
-    //   .is_background = true,
-    //   .num_commands = 5,
-    //   .commands = {
-    //     { "cat", "mutual_aid.txt", "basic.ics", "means_and_ends.pdf", NULL }
-    //     // index 0 { "grep", "comrade", NULL } // index 1 { "wc", "-l, "-c",
-    //     "NULL" } // index 2 { "cat", "NULL" } // index 3 { "sleep", "10",
-    //     "NULL" } // index 4
-    //   }
-    // }
     int result = parse_command(command, &cmd);
     if (result != 0) {
       fprintf(stderr, "error parsing command\n");
       continue;
     }
+    // fork
+    pid_t process_id = fork();
+    if (process_id < 0) {
+      // Bail on failure
+      exit(EXIT_FAILURE);
+    } else if (process_id == 0) {
+      // This is the child
 
-    // Pull out into function
-    for (int i = 0; i < cmd->num_commands; i++) {
-      fprintf(stderr, "execute command %i: %s\n", i, cmd->commands[0][0]);
+      for (int i = 0; i < cmd->num_commands; i++) {
+        fprintf(stderr, "execute command %i: %s\n", i, cmd->commands[0][0]);
 
-      //   int command_items = count_items(cmd->commands[0]);
-      execvp(*cmd->commands[i], *cmd->commands);
+        //   int command_items = count_items(cmd->commands[0]);
 
-      // 1. get_cmd_args(cmd, &cmdArgs)
-      // Take in 1 - len(cmd->commands[0])-1
-      // 2. Fork and wait?
-      // 3. execvp(*cmd->commands[i] + cmdArgs)
+        // The child runs this command and dies
+        execvp(*cmd->commands[i], *cmd->commands);
+        fprintf(stderr, "%s \n", "Execvp failed horribly");
+
+        // 1. get_cmd_args(cmd, &cmdArgs)
+        // Take in 1 - len(cmd->commands[0])-1
+        // 2. Fork and wait?
+        // 3. execvp(*cmd->commands[i] + cmdArgs)
+      }
+    } else {
+      // Parent process
+      printf("Parent process waiting for child to terminate\n");
+      int status;
+      // Wait for the child process to terminate
+      pid_t child_pid = waitpid(process_id, &status, 0);
+      if (child_pid == -1) {
+        perror("waitpid");
+        exit(EXIT_FAILURE);
+      }
+      if (WIFEXITED(status)) {
+        printf("Child process exited with status %d\n", WEXITSTATUS(status));
+      } else if (WIFSIGNALED(status)) {
+        printf("Child process terminated by signal %d\n", WTERMSIG(status));
+      }
+      printf("Parent process continuing execution\n");
     }
 
     // This should work...might bite us later???
