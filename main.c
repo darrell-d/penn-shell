0fbadd75870364741bcbf73c320f85f03e66889f
Darrell De Freitas <ddd@seas.upenn.edu>
2024-02-13 21:41:28 -0500

    progress

--- main.c
+++ main.c
@@ -3,12 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "helpers.h"
 #include "parser.h"
 
 #ifndef PROMPT
 #define PROMPT "penn-shredder# "
 #endif
 
+// 1. Run and wait for input from std in X
+// Block and wait for input from stdin X
+// while not sig kill (or other ending sequence)
+// Listen for input
+// Recieve input
+//                  2. Parse out the commands (just a function call)
+// 3. Run the commands
+// execev
+// start by executing ls command
+// run the cat command with input
+// .. other things
 int main() {
   char command[BUF_LEN] = {0};
 
@@ -30,13 +42,33 @@ int main() {
     command[res] = '\0';
 
     struct parsed_command* cmd = NULL;
+
+    // struct parsed_command {
+    //   .is_background = true,
+    //   .num_commands = 5,
+    //   .commands = {
+    //     { "cat", "mutual_aid.txt", "basic.ics", "means_and_ends.pdf", NULL }
+    //     // index 0 { "grep", "comrade", NULL } // index 1 { "wc", "-l, "-c",
+    //     "NULL" } // index 2 { "cat", "NULL" } // index 3 { "sleep", "10",
+    //     "NULL" } // index 4
+    //   }
+    // }
     int result = parse_command(command, &cmd);
     if (result != 0) {
       fprintf(stderr, "error parsing command\n");
       continue;
     }
 
-    print_parsed_command(stderr, cmd);
+    // Pull out into function
+    for (int i = 0; i < cmd->num_commands; i++) {
+      fprintf(stderr, "execute command %i: %s\n", i, *cmd->commands[0][0]);
+      // 1. get_cmd_args(cmd, &cmdArgs)
+      // 2. Fork and wait?
+      // 3. execvp(*cmd->commands[i] + cmdArgs)
+    }
+
+    // This should work...might bite us later???
+    // print_parsed_command(stderr, cmd);
     fprintf(stderr, "\n");
     free(cmd);
   }
