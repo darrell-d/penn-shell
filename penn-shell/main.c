4f24975f2494269a5e43c3f9d9168991e3351e52
Emily Paul <52010064+em-paul@users.noreply.github.com>
2024-02-18 18:43:38 -0500

    clean up

--- penn-shell/main.c
+++ penn-shell/main.c
@@ -22,17 +22,6 @@
 #define READ 0
 #define WRITE 1
 
-// 1. Run and wait for input from std in X
-// Block and wait for input from stdin X
-// while not sig kill (or other ending sequence)
-// Listen for input
-// Recieve input
-//                  2. Parse out the commands (just a function call)
-// 3. Run the commands
-// execev
-// start by executing ls command
-// run the cat command with input
-// .. other things
 int main() {
   char command[BUF_LEN] = {0};
 
@@ -89,21 +78,6 @@ int main() {
       // run the first command and die
       execvp(*cmd->commands[0], *cmd->commands);
       fprintf(stderr, "%s \n", "Execvp failed horribly");
-
-      // for (int i = 0; i < cmd->num_commands; i++) {
-      //   fprintf(stderr, "execute command %i: %s\n", i, cmd->commands[0][0]);
-
-      //   //   int command_items = count_items(cmd->commands[0]);
-
-      //   // The child runs this command and dies
-      //   execvp(*cmd->commands[i], *cmd->commands);
-      //   fprintf(stderr, "%s \n", "Execvp failed horribly");
-
-      //   // 1. get_cmd_args(cmd, &cmdArgs)
-      //   // Take in 1 - len(cmd->commands[0])-1
-      //   // 2. Fork and wait?
-      //   // 3. execvp(*cmd->commands[i] + cmdArgs)
-      // }
     }
 
     // now both parent and child#1 should close the write end of the pipe
@@ -126,16 +100,6 @@ int main() {
       }
       printf("Parent process continuing execution\n");
 
-      // // close the parent's read end of child#1's pipe
-      // close(pipe1_fds[READ]);
-
-      // // make a pipe for child#2
-      // int pipe2_fds[2];
-      // if (pipe(pipe2_fds) < 0) {
-      //   perror("pipe error");
-      //   exit(EXIT_FAILURE);
-      // }
-
       // fork child#2
       pid_t process_id2 = fork();
       if (process_id2 < 0) {
@@ -143,13 +107,6 @@ int main() {
         exit(EXIT_FAILURE);
       } else if (process_id2 == 0) {
         // This is child #2
-
-        // child #2 should close its write end of pipe1
-        close(pipe1_fds[WRITE]);
-
-        // // child #2 should close its write end of pipe2
-        // close(pipe2_fds[WRITE]);
-
         // redirect standard in to read from the first pipe
         int ret = dup2(pipe1_fds[READ], STDIN_FILENO);
         if (ret < 0) {
@@ -157,12 +114,12 @@ int main() {
           exit(EXIT_FAILURE);
         }
 
-        // If there's a second command, run it using the output from the first command (coming from the parent via pipe2) as input
+        // If there's a second command, run it using the output from the first command (coming from the parent via pipe1) as input
         if (cmd->num_commands > 1) {
           execvp(*cmd->commands[1], *cmd->commands);
           fprintf(stderr, "%s \n", "Execvp failed horribly");
         } else {
-          // If there's no second command, just run `cat` using the output from the first command (coming from the parent via pipe2) as input
+          // If there's no second command, just run `cat` using the output from the first command (coming from the parent via pipe1) as input
           char *argv[] = {"cat", NULL};
           execvp(argv[0], argv);
           fprintf(stderr, "%s \n", "Execvp failed horribly");
@@ -171,9 +128,6 @@ int main() {
 
       // now both parent and child#2 should close the read end of the first pipe
       close(pipe1_fds[READ]);
-
-      // // now both parent and child#2 should close the read end of the pipe
-      // close(pipe2_fds[READ]);
 
       if (process_id2 > 0) {
         // Parent process
@@ -191,14 +145,9 @@ int main() {
           printf("Child process #2 terminated by signal %d\n", WTERMSIG(status));
         }
         printf("Parent process continuing execution\n");
-
-        // // close the parent's write end of child#2's pipe
-        // close(pipe2_fds[WRITE]);
       }
     }
 
-    // This should work...might bite us later???
-    // print_parsed_command(stderr, cmd);
     fprintf(stderr, "\n");
     free(cmd);
   }
